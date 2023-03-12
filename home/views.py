989d89eedbca5055a830f270f6fa998b7d390b21
Thejus-M <git@tigerraw22.anonaddy.com>
2023-03-12 22:26:02 +0530

    feat: create project page and made it look good

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def home(request):
-    return render(request,'base.html',context={})
+    return render(request,'home/home.html',context={})
+
+def projects(request):
+    model=Project.objects.all()
+    return render (request,'home/projects.html',context={'project':model})
